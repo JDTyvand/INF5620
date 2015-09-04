582db180d830bfb3240e7adeec1546ffbf4af7fc
JDTyvand <jorgentyvand@gmail.com>
2015-09-04 12:59:45 +0200

    New and updated files.

--- elastic_pendulum.py
+++ elastic_pendulum.py
@@ -12,67 +12,54 @@ def simulate(
     plot=True,                # make plots or not
     ):
     
+    Theta_s = Theta
     Theta = Theta*pi/180
-    """
-    P = 2*pi
-    T = P*num_periods
+    
+    omega = 1.
+    P = 2*pi/omega
     dt = P/time_steps_per_period
-    Nt = T/dt
+    T = num_periods*P
+    Nt = num_periods*time_steps_per_period
     x = zeros(Nt+1)
     y = zeros(Nt+1)
     t = linspace(0, T, Nt+1)
     x[0] = (1 + epsilon)*sin(Theta)
     y[0] = 1 - (1 + epsilon)*cos(Theta)
     L = sqrt(x[0]**2 + (y[0] - 1)**2)
-    x[1] = x[0] - 0.5*(dt**2)*(beta/(1 - beta))*(1 - (beta/L))*x[0]
-    y[1] = y[0] - 0.5*(dt**2)*(beta/(1 - beta))*(1 - (beta/L))*(y[0] - 1) - (dt**2/2)*beta
+    C = beta/(1-beta)*(1 - beta/L)
+    x[1] = (1 - 0.5*C*dt**2)*x[0]
+    y[1] = (1 - 0.5*C*dt**2)*y[0] + 0.5*(C - beta)*dt**2
 
-    for i in range(len(x)-1):
-   		L = sqrt(x[i]**2 + (y[i] - 1)**2)
-   		x[i+1] = 2*x[i] - x[i-1] - dt**2*(beta/(1 - beta))*(1 - beta/L)*x[i]
-   		y[i+1] = 2*y[i] - y[i-1] - dt**2*(beta/(1 - beta))*(1 - beta/L)*(y[i] - 1) - (dt**2*beta)
-   	"""
-
-    ic = [0,                          # x’=vx
-         (1 + epsilon)*sin(Theta),       # x
-         0,                              # y’=vy
-         1 - (1 + epsilon)*cos(Theta),   # y
-         ]
-
-    def f(u, t, beta):
-        vx, x, vy, y = u
-        L = np.sqrt(x**2 + (y-1)**2)
-        h = beta/(1-beta)*(1 - beta/L)  # help factor
-        return [-h*x, vx, -h*(y-1) - beta, vy]
-
-    # Non-elastic pendulum (scaled similarly in the limit beta=1)
-    # solution Theta*cos(t)
-    P = 2*pi
-    dt = P/time_steps_per_period
-    T = num_periods*P
-    omega = 2*pi/P
-    time_points = np.linspace(
-        0, T, num_periods*time_steps_per_period+1)
+    for i in range(1,len(x)-1):
+      L = sqrt(x[i]**2 + (y[i] - 1)**2)
+      C = beta/(1-beta)*(1 - beta/L)
+      x[i+1] = (2 - C*dt**2)*x[i] - x[i-1]
+      y[i+1] = (2 - C*dt**2)*y[i] - y[i-1] + (C - beta)*dt**2
     
-    solver = odespy.EulerCromer(f, f_args=(beta,))
-    solver.set_initial_condition(ic)
-    u, t = solver.solve(time_points)
-    x = u[:,1]
-    y = u[:,3]
-
     theta = arctan(x/(1-y))
 
     if plot == True:
-   		plt.figure()
-   		plt.plot(x, y)
-   		plt.gca().set_aspect('equal')
-   		plt.figure()
-   		plt.plot(t, theta*180/pi)
-   		plt.show()
-    if abs(Theta) < 10*pi/180:
-      theta_exact = Theta*cos(t)
       plt.figure()
-      plt.plot(t, theta, t, theta_exact)
+      plt.plot(x, y, 'b-')
+      plt.title('Motion of pendulum')
+      plt.xlabel('x')
+      plt.ylabel('y')
+      plt.savefig('Motion_%d.png' % Theta_s)
+      #plt.gca().set_aspect('equal')
+      plt.figure()
+      plt.plot(t, theta*180/pi, 'b-')
+      plt.title('Angle in degrees')
+      plt.xlabel('t')
+      plt.ylabel('theta')
+      plt.savefig('Angle_%d.png' % Theta_s)
+    if abs(Theta) < 10*pi/180:
+      theta_ne = Theta*cos(t)
+      plt.figure()
+      plt.plot(t, theta, t, theta_ne)
+      plt.title('Angle elastic vs. non-elastic')
+      plt.xlabel('t')
+      plt.ylabel('theta')
+      plt.savefig('Elastic_vs_non_%d.png' % Theta_s)
 
     return x, y, theta, t
 
@@ -80,36 +67,40 @@ def pure_vertical():
     beta = 0.9
     omega = sqrt(beta/(1-beta))
     P = 2*pi/omega
-    N = 5
+    N = 6
     num_periods = N/omega
     n = 600
     time_steps_per_period = omega*n
+    epsilon = 0.1
 
     def y_exact(t):
-      return -0.1*cos(omega*t)
+      """
+      Returns the exact solution for the position of y,
+      with a minus sign due to a positive epsilon stretching
+      the pendulum in the negative y direction.
+      """
+      return -epsilon*cos(omega*t)
 
-    x, y, theta, t = simulate(Theta=0, epsilon=0.1, num_periods=num_periods, time_steps_per_period=time_steps_per_period, plot=False)
-    tol = 0.0005
-    assert abs(x.max()) < tol
+    x, y, theta, t = simulate(Theta=0, epsilon=epsilon, num_periods=num_periods, time_steps_per_period=time_steps_per_period, plot=True)
+    tol = 0.0001
     y_e = y_exact(t)
+    assert abs(x.max()) < tol
     err = abs(y_e - y).max()
-    print err
     assert err < tol
 
 def zero_test():
     x, y , theta, t = simulate(Theta=0)
-    print x.max()
-    print y.max()
     if abs(x.max()) > 0 or abs(y.max()) > 0:
 		  print "Error in the numerical scheme!"
     else:
 		  print "Theta = 0 and epsilon = 0 gives x = y = 0 for all times, as intended."
 
-def demo(beta=0.999, Theta=40, num_periods=3):
+def demo(beta=0.9, Theta=60, num_periods=3):
     x, y, theta, t = simulate(beta=beta, Theta=Theta, num_periods=num_periods, time_steps_per_period=600, plot=True)
 def main():
-    #simulate()
-    #zero_test()
-    #pure_vertical()
+    simulate()
+    simulate(Theta=8)
+    zero_test()
+    pure_vertical()
     demo()
 main()
